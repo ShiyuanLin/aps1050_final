bddee44ff91632790bf4056a6cb6ed3f8d27c57f
Hu Kaixuan <hukaixuan0623@gmail.com>
2020-08-24 03:10:52 -0400

    add more features, might need to add a new contract from keeping track of the pet that each user has adopted

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 App = {
   web3Provider: null,
   contracts: {},
+  // gobal variable to keep track of the pets that each user has adopted.
+  // usersPets: {},
+  // TODO: Might be a new contract for tracking the pet that each user has adopted.
 
   init: async function() {
     // Load pets.
@@ -19,16 +22,11 @@ App = {
         petsRow.append(petTemplate.html());
       }
     });
-
     return await App.initWeb3();
   },
 
   initWeb3: async function() {
-    /*
-     * First Added
-     * Replace me...
-     */
-    // Modern dapp browsers...
+
     if (window.ethereum) {
       App.web3Provider = window.ethereum;
       try {
@@ -47,16 +45,32 @@ App = {
     else {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
+
     web3 = new Web3(App.web3Provider);
+
+    /**
+    * Feature: Show current user and balance.
+    */
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+      const account = accounts[0];
+      $('.currUser').text("Current user: " + account);
+      web3.eth.getBalance(account, function(error, balance) {
+          if (error) {
+            console.log(error);
+          }
+          var currBalance = web3.fromWei(balance.toNumber(), 'Ether')
+          $('.currBalance').text("Current balance: " + currBalance + ' ETH')
+      });
+    });
+
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * 2nd Added
-     * Replace me...
-     */
     $.getJSON('Adoption.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
       var AdoptionArtifact = data;
@@ -77,22 +91,19 @@ App = {
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * 3rd Added
-     * Replace me...
-     */
     var adoptionInstance;
     var num_adopted = 0;
     var clients = new Set();
 
-    $('#petsAdoptedNum').text("# of pets bought: " + num_adopted)
-    $('#clientsAdoptedNum').text("# of clients: " + clients.size)
+    $('#petsAdoptedNum').text("Total number of pets adopted: " + num_adopted)
+    $('#clientsAdoptedNum').text("Total number of clients: " + clients.size)
 
     App.contracts.Adoption.deployed().then(function(instance) {
       adoptionInstance = instance;
 
       return adoptionInstance.getAdopters.call();
     }).then(function(adopters) {
+      // adopters is adoptionInstance.getAdopters.call()
       for (i = 0; i < adopters.length; i++) {
         if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
           num_adopted++;
@@ -100,13 +111,25 @@ App = {
           $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
+
+      web3.eth.getAccounts(function(error, accounts) {
+        if (error) {
+          console.log(error);
+        }
+        web3.eth.getBalance(accounts[0], function(error, balance) {
+            if (error) {
+              console.log(error);
+            }
+            var currBalance = web3.fromWei(balance.toNumber(), 'Ether')
+            $('.currBalance').text("Current balance: " + currBalance + ' ETH')
+        });
+      });
+
       /**
-      * Feature 2: Number of pets adopted, number of clients
+      * Feature 2: Record number of pets adopted, number of clients
       */
-      // console.log("num_adopted: ", num_adopted)
-      // console.log("num_clients: ", clients.size)
-      $('#petsAdoptedNum').text("# of pets bought: " + num_adopted)
-      $('#clientsAdoptedNum').text("# of clients: " + clients.size)
+      $('#petsAdoptedNum').text("Total number of pets adopted: " + num_adopted)
+      $('#clientsAdoptedNum').text("Total number of clients: " + clients.size)
     }).catch(function(err) {
       console.log(err.message);
     });
@@ -115,18 +138,38 @@ App = {
   handleAdopt: function(event) {
     event.preventDefault();
 
+    var petNameList = ["Frieda", "Gina", "Collins", "Melissa", "Jeanine", "Elvia",
+                       "Latisha", "Coleman", "Nichole", "Fran", "Leonor", "Dean",
+                       "Stevenson", "Kristina", "Ethel", "Terry"];
+
     var petId = parseInt($(event.target).data('id'));
+    var petName = petNameList[petId];
 
     /**
-    * Feature 1: Disable button during process
+    * Feature 1: Disable button during processing
     *
     */
-    $(this).text('Signing..').attr('disabled', true);
+    // $(this).text('Signing...').attr('disabled', true);
+    $('.panel-pet').eq(petId).find('button').text('Signing...').attr('disabled', true);
 
-    /*
-     * 4th Added
-     * Replace me...
-     */
+    // Prevent users from adopting pet if they cannot afford the handling fee (gas fee).
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+      web3.eth.getBalance(accounts[0], function(error, balance) {
+          if (error) {
+            console.log(error);
+          }
+          var currBalance = web3.fromWei(balance.toNumber(), 'Ether')
+          console.log(currBalance);
+          if (currBalance==0) {
+              $('.noBalance').text("You don't have enough balance for the pet adoption handling fee (please switch account or add more balance).").css("color", "red");
+              $('.panel-pet').eq(petId).find('button').text('Adopt').attr('disabled', true);
+          }
+      });
+    });
+
     var adoptionInstance;
 
     web3.eth.getAccounts(function(error, accounts) {
@@ -142,13 +185,17 @@ App = {
         // Execute adopt as a transaction by sending account
         return adoptionInstance.adopt(petId, {from: account});
       }).then(function(result) {
-        return App.markAdopted();
+          // Show the name of the pet the user have just adopted.
+          $('.alert').text('You have successfully adopted ' + petName + '!');
+
+          return App.markAdopted();
       }).catch(function(err) {
-        console.log(err.message);
+          // Enable button if there is error during processing
+          $(this).text('Adopt').removeAttr('disabled');
+          console.log(err.message);
       });
     });
   }
-
 };
 
 $(function() {
